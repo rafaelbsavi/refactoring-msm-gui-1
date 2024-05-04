ecb5e177b23b4913ff3015d42a64198b36ef3353
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-05-04 22:31:08 +0000

    vF

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,11 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
-end
+
+  def characters
+    my_id = self.id
+    mathching_characters = Character.where({ :actor => my_id})
+    return mathching_characters
+  end
+
+  end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,18 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    d_id = self.director_id
+    director = Director.where({ :id => d_id}).at(0)
+    return director
+  end
+
+  def characters
+    my_id = self.id
+    mathching_characters = Character.where({ :movie_id => my_id})
+    return mathching_characters
+  end
+
+
 end
